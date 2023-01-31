492ea55392b7ae44d21f6af9e474ca4aac24b046
Sid <35641391+LateralFricativeEnjoyer@users.noreply.github.com>
2023-01-30 17:12:45 -0800

    Create randompage.js

---
+++ wiki/randompage.js
@@ -0,0 +1,6 @@
+pages = [
+  "tlnxad", "aarnor", "baneikh", "dlhen", "dhehenevari", "dhyczul", "duas_zadufhe", "frisu", "galat", "gitiidhnae", "gylhev", "gylhev_calendar", "gzokangbyr", "haliiutha", "iienaartsuu", "khadmot", "khaneu", "khe_ionalu", "khinixa_ahuire_outpost", "dialects_uvioluhu", "kholivh", "kholivh_calendar", "libhemora", "lhar_nucuomk"
+];
+
+pagenumber = Math.floor(Math.random() * pages.length);
+window.location.replace("http://sidlangs.com/wiki/" + pages[pagenumber]);
